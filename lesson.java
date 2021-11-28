5c7348af28a39f23f496f5e61a12a7149cd914f5
bekzat <bekzat46@mail.ru>
2021-11-29 00:55:39 +0600

    triangle

--- lesson.java
+++ lesson.java
@@ -1,5 +1,21 @@
+class Triangle{
+    String str;
+    Triangle(String str){
+        this.str = str;
+    }
+    void Trngl(){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(str);
+            }
+            System.out.println();
+        }
+    }
+}
+
 class lesson{
     public static void main(String args[]){
-        System.out.print("GitHub");
+        Triangle myTriangle = new Triangle("s");
+        myTriangle.Trngl();
     }
 }
